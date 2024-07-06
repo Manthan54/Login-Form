aaeecc2f4f76327084f388decf2f9c97f5f45af6
Manthan Chandravijay Jagtap <133759783+Manthan54@users.noreply.github.com>
2024-07-06 22:56:11 +0530

    Add files via upload

---
+++ functions/submit-form.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('form');
+const uname = document.getElementById('name');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const date = document.getElementById('birthdate');
+const email = document.getElementById('email');
+
+function check(){
+    alert("Successfully submitted")
+}
