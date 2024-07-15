b34a21fbd93ecf6739383d5654a19aca6b684fd8
Manthan Chandravijay Jagtap <133759783+Manthan54@users.noreply.github.com>
2024-07-16 00:25:53 +0530

    Add files via upload

---
+++ submit-form.js
@@ -0,0 +1,20 @@
+export function validateForm() {
+    const name = localStorage.getItem('name');
+    const gender = localStorage.getItem('gender');
+    const birthdate = localStorage.getItem('birthdate');
+  
+    const formData = {
+      name,
+      gender,
+      birthdate
+    };
+  
+    const jsonData = JSON.stringify(formData);
+    const blob = new Blob([jsonData], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'form_data.json';
+    link.textContent = 'Download Form Data';
+    document.body.appendChild(link);
+  }
