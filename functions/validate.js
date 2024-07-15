ed62cd1bb26ec65a3da737faf712d25682748773
Manthan Chandravijay Jagtap <133759783+Manthan54@users.noreply.github.com>
2024-07-16 00:37:34 +0530

    Add files via upload

---
+++ functions/validate.js
@@ -0,0 +1,20 @@
+function validateForm(){
+    const name = document.getElementById('name').value;
+    const gender = document.getElementById('gender').value;
+    const birthdate = document.getElementById('birthdate').value;
+
+    if (name === '' || gender === '' || birthdate === ''){
+        alert('All fields are required');
+        return false;
+    }
+
+    localStorage.setItem('name', name);
+    localStorage.setItem('gender', gender);
+    localStorage.setItem('birthdate', birthdate);
+
+    window.location.href = 'welcome.html';
+    return false;
+}
+
+document.getElementById('loginForm').addEventListener('submit', validateForm);
+
